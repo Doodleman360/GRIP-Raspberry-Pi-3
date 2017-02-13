1283acd18533077ced6ddaa068a167e185c86ffc
Doodleman360 <julislo123@gmail.com>
2017-02-13 14:05:51 -0700

    removed unessasary class

--- USB/usb_vision.py
+++ USB/usb_vision.py
@@ -8,7 +8,6 @@ import cv2
 import networktables
 from networktables import NetworkTable
 from usb_GRIP import GripPipeline
-#from grip import GripPipeline
 import os
 import sys
 import logging
@@ -29,7 +28,6 @@ def extra_processing(pipeline):
     # Find the bounding boxes of the contours to get x, y, width, and height
     for contour in pipeline.filter_contours_output:
         x, y, w, h = cv2.boundingRect(contour)
-        #if (2 < (h/w)) & ((h/w) < 3):
         center_x.append(x + w / 2)  # X and Y are coordinates of the top-left corner of the bounding box
         center_y.append(y + h / 2)
         widths.append(w)
@@ -39,8 +37,6 @@ def extra_processing(pipeline):
         # Publish to the '/vision' network table
         pti = (widths[0] / 5 + heights[0] / 2) / 2
         table.putValue("pti", pti)
-        #table.putValue("width", pipeline.resize_image_width)
-        #table.putValue("height", pipeline.resize_image_height)
         table.putValue("r1cX", center_x[0])
         table.putValue("r1cY", center_y[0])
         table.putValue("r1w", widths[0]/pti)
