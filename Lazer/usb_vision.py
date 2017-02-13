73382c45f9f0e923acc06b0cfe657977f3c9f224
Doodleman360 <julislo123@gmail.com>
2017-02-13 14:00:48 -0700

    changed rect to circle

--- Lazer/usb_vision.py
+++ Lazer/usb_vision.py
@@ -28,22 +28,19 @@ def extra_processing(pipeline):
     # Find the bounding boxes of the contours to get x, y, width, and height
     for contour in pipeline.filter_contours_output:
         x, y, w, h = cv2.boundingRect(contour)
-        if (2 < (h/w)) & ((h/w) < 3):
-            center_x.append(x + w / 2)  # X and Y are coordinates of the top-left corner of the bounding box
-            center_y.append(y + h / 2)
-            widths.append(w)
-            heights.append(y)
+        center_x.append(x + w / 2)  # X and Y are coordinates of the top-left corner of the bounding box
+        center_y.append(y + h / 2)
+        widths.append(w)
+        heights.append(y)
     table = NetworkTable.getTable("/vision")
     if len(pipeline.filter_contours_output) > 1:
         # Publish to the '/vision' network table
-        pti = (widths[0] / 5 + heights[0] / 2) / 2
-        table.putValue("pti", pti)
         table.putValue("width", pipeline.resize_image_width)
         table.putValue("height", pipeline.resize_image_height)
-        table.putValue("r1cX", center_x[0])
-        table.putValue("r1cY", center_y[0])
-        table.putValue("r1w", widths[0]/pti)
-        table.putValue("r1h", heights[0]/pti)
+        table.putValue("cX", center_x[0])
+        table.putValue("cY", center_y[0])
+        table.putValue("w", widths[0])
+        table.putValue("h", heights[0])
         table.putValue("locked", True)
     else:
         table.putValue("locked", False)
@@ -89,7 +86,7 @@ def main():
             ret, frame = cap.read()
             for contour in pipeline.filter_contours_output:
                 x, y, w, h = cv2.boundingRect(contour)
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 255), 2)
+                cv2.circle(frame, (x, y), (((w/2)+(h/2))/2), (255, 255, 255), 2)
             cv2.imshow('frame', frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
